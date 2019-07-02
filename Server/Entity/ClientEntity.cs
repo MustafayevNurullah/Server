28f64e3b5ec96cbb90895b7fb13bb783f3d1d067
nurlan3295 <nurlan3295@gmail.com>
2019-07-02 22:00:26 +0400

    Add project files.

---
+++ Server/Entity/ClientEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Entity
+{
+    public class ClientEntity
+    {
+        public string Name { get; set;}
+        public string Ip { get; set; }
+    }
+}
---
+++ Server/ViewModel/ServerVIewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.ViewModel
+{
+   public class ServerVIewModel:BaseViewModel
+    {
+        public ServerVIewModel()
+        {
+            IPEndPoint endp = new IPEndPoint(IPAddress.Parse("10.1.16.13"), 1031);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(endp);
+            socket.Listen(10);
+
+
+
+        Task receiver = Task.Run(() =>
+        {
+            while (true)
+            {
+                int length = client.Receive(buffer);
+                Console.WriteLine("Client  " + Encoding.ASCII.GetString(buffer, 0, length));
+            }
+
+        });
+
+        }
+    }
+}
